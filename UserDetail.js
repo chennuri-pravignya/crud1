16780d3898692001a1e324aeea4239aa63eaf3fb
chennuri-pravignya <pravignyachennuri@gmail.com>
2024-10-05 14:35:23 +0530

    UserDetail.js

---
+++ UserDetail.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+
+const UserDetail = () => {
+  const { id } = useParams();
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then(response => {
+        setUser(response.data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the user!', error);
+        setLoading(false);
+      });
+  }, [id]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!user) {
+    return <div>User not found</div>;
+  }
+
+  return (
+    <div>
+      <h1>{user.name}</h1>
+      <p>Email: {user.email}</p>
+      <p>Phone: {user.phone}</p>
+      <p>Username: {user.username}</p>
+      <p>Address: {user.address.street}, {user.address.city}</p>
+      <p>Company: {user.company.name}</p>
+      <p>Website: {user.website}</p>
+    </div>
+  );
+};
+
+export default UserDetail;
